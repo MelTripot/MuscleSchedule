79f9341a8a1231786de77f14f6050d763bf6334a
Meltripot <tripot.melanie@gmail.com>
2022-02-18 11:37:09 +0100

    page par default

--- muscle_schedule/lib/widget/navgbar.dart
+++ muscle_schedule/lib/widget/navgbar.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Navbar extends StatefulWidget {
-  const Navbar({Key? key}) : super(key: key);
+  Navbar({Key? key}) : super(key: key);
 
   @override
   State<Navbar> createState() => _NavbarState();
+
+  String Page = '';
 }
 
 class _NavbarState extends State<Navbar> {
