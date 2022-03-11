813da04959fd85f0801742d7e06214aa5c678416
Meltripot <tripot.melanie@gmail.com>
2022-03-11 16:19:22 +0100

    cleaning utils

--- muscle_schedule/lib/utils.dart
+++ muscle_schedule/lib/utils.dart
@@ -1,39 +1,7 @@
-// Copyright 2019 Aleksander Woźniak
-// SPDX-License-Identifier: Apache-2.0
-
 import 'dart:collection';
 
 import 'package:muscle_schedule/pages/workout.dart';
 import 'package:table_calendar/table_calendar.dart';
-
-/// Example event class.
-class Event {
-  final String title;
-
-  const Event(this.title);
-
-  @override
-  String toString() => title;
-}
-
-/// Example events.
-///
-/// Using a [LinkedHashMap] is highly recommended if you decide to use a map.
-final kWorkOut = LinkedHashMap<DateTime, List<Event>>(
-  equals: isSameDay,
-  hashCode: getHashCode,
-)..addAll(_kWorkOutource);
-
-final _kWorkOutource = Map.fromIterable(List.generate(50, (index) => index),
-    key: (item) => DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5),
-    value: (item) => List.generate(
-        item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
-  ..addAll({
-    kToday: [
-      Event('Today\'s Event 1'),
-      Event('Today\'s Event 2'),
-    ],
-  });
 
 int getHashCode(DateTime key) {
   return key.day * 1000000 + key.month * 10000 + key.year;
