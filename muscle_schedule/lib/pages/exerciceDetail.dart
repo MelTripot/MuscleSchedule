f4231af0ac6abe6dcb708d0583c462c7cd10625f
Meltripot <tripot.melanie@gmail.com>
2022-03-13 18:34:37 +0100

    Description Working

--- muscle_schedule/lib/pages/exerciceDetail.dart
+++ muscle_schedule/lib/pages/exerciceDetail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:muscle_schedule/pages/workout.dart';
 import 'package:muscle_schedule/services/wgerApi.dart';
+import 'package:html/parser.dart';
 
 class ExerciceDetailsPage extends StatefulWidget {
   final Exercice exo;
@@ -11,31 +12,34 @@ class ExerciceDetailsPage extends StatefulWidget {
 }
 
 class _ExerciceDetailsPageState extends State<ExerciceDetailsPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    if (widget.exo.description != null) {
+      final document = parse(widget.exo.description!);
+      final String parsedString =
+          parse(document.body!.text).documentElement!.text;
+      widget.exo.description = parsedString.toString();
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.exo.name),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             const Text(
-              'On va mettre une liste la',
+              'Description  :',
+              style: TextStyle(
+                fontSize: 18,
+              ),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+              widget.exo.description!,
+              style: const TextStyle(
+                fontSize: 26,
+              ),
+            )
           ],
         ),
       ),
